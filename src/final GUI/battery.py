fe97ccf4348ed24f1b395b8d4336439e08682b48
Jfhseh <67591670+Jfhseh@users.noreply.github.com>
2022-08-05 20:40:30 -0400

    Create battery.py

---
+++ src/final GUI/battery.py
@@ -0,0 +1,13 @@
+import requests;
+def noBattery():
+		"""
+		sends a post request to the server, telling it that this device has less than 1% of battery
+		"""
+		params = {
+			"type":"lowBattery",
+			"battery":"0"
+		};
+		x = requests.post("https://script.google.com/macros/s/AKfycbwTPcKwdGHbR4iwxnD_OQ6l6a7v_9f8F9Q0VvPpTN0NLHwln8kGU332h9gp6Wr_Nup3_Q/exec",params=params);
+		print(x.text);
+		print(x.status_code);
+noBattery();
